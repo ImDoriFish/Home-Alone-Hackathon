a0d5dcc866719e86265155443c71618dfe0b51a0
Khang Le <lebaokhanghcm@gmail.com>
2026-04-12 00:14:42 -0500

    Door interaction

---
+++ Assets/DoorInteract.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using TMPro;
+
+public class DoorInteract : MonoBehaviour
+{
+    public bool isOpen = true;
+    public Collider2D doorBlocker;
+    public GameObject statusPanel;
+    public TextMeshProUGUI statusText;
+
+    private bool playerNearby = false;
+
+    void Start()
+    {
+        
+        UpdateDoor();
+        HideStatus();
+    }
+
+    void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            isOpen = !isOpen;
+            UpdateDoor();
+            ShowStatus(isOpen ? "Door Unlocked" : "Door Locked");
+        }
+    }
+
+    void UpdateDoor()
+    {
+        if (doorBlocker != null)
+        {
+            doorBlocker.enabled = !isOpen;
+        }
+    }
+
+    void ShowStatus(string message)
+    {
+        if (statusText != null)
+        {
+            statusText.text = message;
+        }
+
+        if (statusPanel != null)
+        {
+            statusPanel.SetActive(true);
+
+            CancelInvoke(nameof(HideStatus));
+            Invoke(nameof(HideStatus), 2f);
+        }
+    }
+
+    void HideStatus()
+    {
+        if (statusPanel != null)
+        {
+            statusPanel.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+        }
+    }
+}
